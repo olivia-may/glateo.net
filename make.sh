0c5084255e71f86bbe4a1267bf38bdaad2638681
Olivia May <olivia.may@tuta.io>
2023-11-29 16:35:47 -0600

    Change English version <header> links and make make.sh look easier to read

--- make.sh
+++ make.sh
@@ -21,11 +21,24 @@ binary_files=(
 
 source_dir=$(pwd)
 
-# args: file_name
-replace_keywords () {
+rm -rf glateo.net/
+mkdir -p $source_dir/glateo.net
+mkdir -p $source_dir/glateo.net/eo
+mkdir -p $source_dir/glateo.net/en
+
+cp ${glateo_files[*]} ${binary_files[*]} $source_dir/glateo.net/
+cd $source_dir/eo/
+cp ${html_files[*]} $source_dir/glateo.net/eo/
+cd $source_dir/en/
+cp ${html_files[*]} $source_dir/glateo.net/en/
+
+### Esperanto
+cd $source_dir/glateo.net/eo
+for file_name in ${html_files[@]}; do
+
     ../../src/rpl '@head-links' '''<link rel="icon" href="pride-flag.ico">
         <link rel="stylesheet" href="../reset.css">
-        <link rel="stylesheet" href="../retpagaro.css">''' $1
+        <link rel="stylesheet" href="../retpagaro.css">''' $file_name
     ../../src/rpl '@header' '''<header>
             <h1>glateo.net</h1>
             <a href="demandoj-kaj-respondoj">Demandoj kaj Respondoj</a>
@@ -36,41 +49,39 @@ replace_keywords () {
             <a href="pri">Pri (ne finita)</a>
             <a href="riismo">Riismo</a>
             <a href="vortaro.php">Vortaro (ne finita)</a>
-        </header>''' $1
+        </header>''' $file_name
     ../../src/rpl '@language-menu' '''<div id="language-menu">
-                <a href='''"\"../eo/$1\""'''>Esperanto</a>
-                <a href='''"\"../en/$1\""'''>English</a>
-            </div>''' $1
+                <a href='''"\"../eo/$file_name\""'''>Esperanto</a>
+                <a href='''"\"../en/$file_name\""'''>English</a>
+            </div>''' $file_name
     ../../src/rpl '@footer' '''<footer>
-        </footer>''' $1
-}
+        </footer>''' $file_name
 
-rm -rf glateo.net/
-
-mkdir -p $source_dir/glateo.net
-
-mkdir -p $source_dir/glateo.net/eo
-
-mkdir -p $source_dir/glateo.net/en
-
-cp ${glateo_files[*]} ${binary_files[*]} $source_dir/glateo.net/
-
-cd $source_dir/eo/
-
-cp ${html_files[*]} $source_dir/glateo.net/eo/
-
-cd $source_dir/en/
-
-cp ${html_files[*]} $source_dir/glateo.net/en/
-
-cd $source_dir/glateo.net/eo
-
-for file_name in ${html_files[@]}; do
-    replace_keywords $file_name
 done
 
+### English
 cd $source_dir/glateo.net/en
-
 for file_name in ${html_files[@]}; do
-    replace_keywords $file_name
+
+    ../../src/rpl '@head-links' '''<link rel="icon" href="pride-flag.ico">
+        <link rel="stylesheet" href="../reset.css">
+        <link rel="stylesheet" href="../retpagaro.css">''' $file_name
+    ../../src/rpl '@header' '''<header>
+            <h1>glateo.net</h1>
+            <a href="demandoj-kaj-respondoj">Questions and Answers</a>
+            <a href="hejmo">Home</a>
+            <a href="icxismo-kaj-ipismo">I&#265ismo and Ipismo</a>
+            <a href="rimedoj">List of all sources</a>
+            <a href="parentismo">Parentismo</a>
+            <a href="pri">About (ne finita)</a>
+            <a href="riismo">Riismo</a>
+            <a href="vortaro.php">Dictionary (ne finita)</a>
+        </header>''' $file_name
+    ../../src/rpl '@language-menu' '''<div id="language-menu">
+                <a href='''"\"../eo/$file_name\""'''>Esperanto</a>
+                <a href='''"\"../en/$file_name\""'''>English</a>
+            </div>''' $file_name
+    ../../src/rpl '@footer' '''<footer>
+        </footer>''' $file_name
+
 done

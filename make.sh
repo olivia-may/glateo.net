11941f7936c1499363c1bf17d70999f3ed272683
Olivia May <olivia.may@tuta.io>
2023-11-29 21:17:31 -0600

    Add new keywords for parentismo and riismo, less repeating html!

--- make.sh
+++ make.sh
@@ -37,10 +37,12 @@ function replace_keywords() {
         <link rel="icon" href="../pride-flag.ico">
         <link rel="stylesheet" href="../reset.css">
         <link rel="stylesheet" href="../retpagaro.css">''' $1
+
     ../../src/rpl '@language-menu' '''<div id="language-menu">
                 <a href='''"\"../eo/$1\""'''>Esperanto</a>
                 <a href='''"\"../en/$1\""'''>English</a>
             </div>''' $1
+            
     ../../src/rpl '@resources' '''<ul>
                 <li><a href="https://tersiso.net/">
                 TerSISO</a></li>
@@ -60,8 +62,76 @@ function replace_keywords() {
                 Seksneutralaj parencovortoj - rezultoj de opinisondo de 
                 Markos Kramer de Lingva Kritiko</a></li>
             </ul>''' $1
+
     ../../src/rpl '@extra-resources' '''<ul>
                     <li><a href="https://www.youtube.com/watch?v=ZyxDjiT3lfw">Ri Liberas</a></li>
+                </ul>''' $1
+
+    ../../src/rpl '@parentismo-roots' '''<ol>
+                    <li>abado - abato</li>
+                    <li>avuo - avo</li>
+                    <li>☆ petolinfano - bubo</li>
+                    <li>eŝo - edzo</li>
+                    <li>grofo - grafo</li>
+                    <li>fianceo - fianĉo</li>
+                    <li>fizo - filo</li>
+                    <li>sahodo - frato</li>
+                    <li>neeŝo - fraŭlo</li>
+                    <li>kido - knabo</li>
+                    <li>kuzeno - kuzo</li>
+                    <li>monĥo - monaĥo</li>
+                    <li>nepoto - nepo</li>
+                    <li>nievo - nevo</li>
+                    <li>ontio - onklo</li>
+                    <li>parento - patro</li>
+                    <li>prenso - princo</li>
+                    <li>rejĝo - reĝo</li>
+                    <li>duĉo - duko</li>
+                    <li>sioro -sinjoro</li>
+                    <li>viduo - vidvo</li>
+                    <li>☆ adolto, ☆ plenkreskulo, ☆ homo - viro</li>
+                </ol>''' $1
+
+    ../../src/rpl '@parentismo-patrismo-example' '''<ol>
+                    <li>
+                        Li estas mia patro.<br>
+                        Ŝi estas mia patrino.<br>
+                        Tiu estas mia (?)patro.<br>
+                    </li>
+                    <li>
+                        Mi havas tri knabojn.<br>
+                        Mi havas tri knabinojn.<br>
+                        Mi havas tri geknabojn.<br>
+                    </li>
+                    <li>
+                        Unu kuzo kaj unu kuzino estas du gekuzoj.
+                    </li>
+                </ol>''' $1
+
+    ../../src/rpl '@parentismo-example' '''<ol>
+                    <li>
+                        Li estas mia parento.<br>
+                        Ŝi estas mia parento.<br>
+                        Ri estas mia parento.<br>
+                    </li>
+                    <li>
+                        Mi havas tri kidojn.<br>
+                    </li>
+                    <li>
+                        Unu kuzeniĉo kaj unu kuzenino estas du gekuzenoj kaj du kuzenoj.
+                    </li>
+                </ol>''' $1
+    
+    ../../src/rpl '@riismo-example' '''<ul>
+                    <li>Mi ne konas la genron de tiu homo, do mi uzas la pronomon
+                    "ri" por ri.</li>
+                    <li>Ri estas neduumulo, ri uzas la pronomon "ri".</li>
+                    <li>Se vi vidus homon, diru saluton al ri.</li>
+                    <li>Ri estas mia patro kaj ri estas mia patrino.</li>
+                    <li>Riaj kukoj estas bongustaj.</li>
+                    <li>Ĉu vi vidas rin?</li>
+                    <li>Mi vidas riajn katojn kaj hundojn.</li>
+                    <li>Vi vidas rian domon.</li>
                 </ul>''' $1
 }
 
@@ -79,6 +149,7 @@ for file_name in ${html_files[@]}; do
             <a href="riismo">Riismo</a>
             <a href="vortaro.php">Vortaro</a>
         </header>''' $file_name
+
     ../../src/rpl '@footer' '''<footer>
             <a href="pri">Pri</a>
             <a href="rimedoj">Listo de Ĉiuj Rimedoj</a>
@@ -100,6 +171,7 @@ for file_name in ${html_files[@]}; do
             <a href="riismo">Riismo</a>
             <a href="vortaro.php">Dictionary</a>
         </header>''' $file_name
+
     ../../src/rpl '@footer' '''<footer>
             <a href="pri">About</a>
             <a href="rimedoj">List of all sources</a>
